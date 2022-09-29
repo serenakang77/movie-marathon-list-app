26d445598cd19f5b2e2118ae8b086db707f3a79d
serenakang <serenakang77@gmail.com>
2022-09-29 16:22:56 -0400

    made searchMovie method API

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 // PSEUDO - CODE
 
 // Create app object (movieMarathonApp)
+const movieMarathonApp = {};
+movieMarathonApp.options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd68a0da33dmshec23235f282deeap141e2bjsn8069a44d4671',
+		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+	}
+};
+// scope our data and methods to movieMarathon app object
+movieMarathonApp.url = "https://movie-database-alternative.p.rapidapi.com/"
+
+movieMarathonApp.searchMovie = () => {
+    const movieMarathonAppUrl = new URL(movieMarathonApp.url);
+    const userInput = "Marvel"
+    
+    movieMarathonAppUrl.search = new URLSearchParams({
+        s: userInput
+    })
+    fetch(movieMarathonAppUrl, movieMarathonApp.options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+} 
+
+movieMarathonApp.searchMovieById = () => {
+
+    const movieMarathonAppUrlById = new URL(movieMarathonApp.url);
+    const userInputById = "tt4154664"
+
+    movieMarathonAppUrlById.search = new URLSearchParams({
+        r: "json",
+        i: userInputById
+    })
+    fetch(movieMarathonAppUrlById, movieMarathonApp.options1)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
+movieMarathonApp.options1 = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'd68a0da33dmshec23235f282deeap141e2bjsn8069a44d4671',
+        'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+    }
+};
 
     // Set searchBar variable to get the userQuery
 
@@ -23,10 +68,14 @@
         // *STRETCH GOAL* - click on movie for more information
 
     // Create init method on movieMarathonApp
-
+    movieMarathonApp.init = () => {
+        movieMarathonApp.searchMovie();
+        movieMarathonApp.searchMovieById();
+    }
         // call searchMovie method
         // call getMovie method (forEach item in searchMovie array)
         // call displayMovies method (for each object getMovie creates)
         // call toggleList method to allow for adding and removing movies from the list
 
     // Call movieMarathonApp.init
+    movieMarathonApp.init();
