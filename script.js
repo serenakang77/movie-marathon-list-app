c09c295dc659d3f4a3bba4f24c9f42021a19960f
serenakang <serenakang77@gmail.com>
2022-09-29 14:53:22 -0400

    Added PSEUDO Code

---
+++ script.js
@@ -0,0 +1,13 @@
+// PSEUDO - CODE
+
+// Create app object 
+    // Create searchBar to get the userQuery
+    // Make on submit event listener on searchBar
+        // Create searchMovie method that makes an API call to the Movie Database for the three most popular movies(use filter) from the results
+
+        // Create getMovie method that takes the id of the movies that user searched and makes an API call for that movies information(by putting ID)
+
+    // Create displayMovie(3 movies) method that manipulate the DOMs to add movie images information
+        // Create the button that shows more movies when the user click it
+
+    // Create toggleList method that adds and remove movie titles to the list when the user clicks + or -
