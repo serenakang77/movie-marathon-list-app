e96a1fa7f231a83e6d29c7886d5f4af069edeb9f
serenakang <serenakang77@gmail.com>
2022-10-02 18:17:02 -0400

    Added displayMovie Method and edited javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // PSEUDO - CODE
 
-// Create app object (movieMarathonApp)
+// Create movieMarathonApp object
 const movieMarathonApp = {};
 movieMarathonApp.options = {
 	method: 'GET',
@@ -12,32 +12,33 @@ movieMarathonApp.options = {
 // scope our data and methods to movieMarathon app object
 movieMarathonApp.url = "https://movie-database-alternative.p.rapidapi.com/"
 
-movieMarathonApp.searchMovie = () => {
+// Create searchMovie method that makes an API call to the Movie Database for the three (or more) most popular movies(using filter function) from the results
+movieMarathonApp.searchMovie = (parameter) => {
     const movieMarathonAppUrl = new URL(movieMarathonApp.url);
-    const userInput = "Marvel"
-    
     movieMarathonAppUrl.search = new URLSearchParams({
-        s: userInput
+        s:parameter
     })
+    
     fetch(movieMarathonAppUrl, movieMarathonApp.options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    .then(response => response.json())
+    .then(response => movieMarathonApp.displayMovie(response))
+    .catch(err => console.error(err));
 } 
 
-movieMarathonApp.searchMovieById = () => {
+// Create getMovie method that takes the id of the movies that user searched and makes an API call for that movie's information (by feeding in the ID), and creates an object with that information
+movieMarathonApp.getMovie = () => {
 
     const movieMarathonAppUrlById = new URL(movieMarathonApp.url);
     const userInputById = "tt4154664"
-
+    
     movieMarathonAppUrlById.search = new URLSearchParams({
         r: "json",
         i: userInputById
     })
     fetch(movieMarathonAppUrlById, movieMarathonApp.options1)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    .then(response => response.json())
+    .then(response => movieMarathonApp.api2 = response)
+    .catch(err => console.error(err));
 }
 movieMarathonApp.options1 = {
     method: 'GET',
@@ -47,35 +48,54 @@ movieMarathonApp.options1 = {
     }
 };
 
-    // Set searchBar variable to get the userQuery
+movieMarathonApp.searchBarSubmitButton = document.querySelector(".search-button");
 
-    // Make an onSubmit event listener on searchBar
+// Create displayMovies method that manipulates the DOM to add movie images and information
+movieMarathonApp.displayMovie = (apiResponse) => {
+    // !! (SERENA WILL WORK ON THIS) Handle any errors from searchMovie before displaying(NEED TO WORK)
+    const ulElement = document.querySelector(".results ul")
+    ulElement.innerHTML = "";
+    for(let i = 0; i<3; i++){
+        const liElement = document.createElement("li");
+        liElement.innerHTML = `
+        <img class="movie-poster" src="${apiResponse.Search[i].Poster}" alt="${apiResponse.Search[i].Title}">
+        <p class="movie-title">${apiResponse.Search[i].Title}</p>
+        <i class="fa fa-plus-circle" aria-hidden="true"></i>
+        <i class="fa fa-minus-circle" aria-hidden="true"></i>
+        `
+        ulElement.append(liElement);
+        // !! (DANA WILL WORK ON THIS) *STRETCH GOAL* - Create an event listener on the arrow buttons that carousels through more movies when the user clicks it(NEED TO WORK)
+    }
+}
 
-        // Create searchMovie method that makes an API call to the Movie Database for the three (or more) most popular movies(using filter function) from the results
+// Create init method on movieMarathonApp
+movieMarathonApp.init = () => {
+    // Make an onSubmit event listener on searchBarElement
+    movieMarathonApp.searchBarSubmitButton.addEventListener("click", () => {
+        // Set searchBarElement variable to get the userQuery
+        movieMarathonApp.searchBarElement = document.querySelector(".search");
 
-        // Create getMovie method that takes the id of the movies that user searched and makes an API call for that movie's information (by feeding in the ID), and creates an object with that information
+        movieMarathonApp.userMovie = movieMarathonApp.searchBarElement.value;
+        movieMarathonApp.searchMovie(movieMarathonApp.userMovie);
+        movieMarathonApp.searchBarElement.value="";
+    })
+}
 
-    // Create displayMovies method that manipulates the DOM to add movie images and information
+// Call movieMarathonApp.init
+movieMarathonApp.init();
 
-        // Handle any errors from searchMovie before displaying
 
-        // *STRETCH GOAL* - Create an event listener on the arrow buttons that carousels through more movies when the user clicks it
+// UNFINISHED PSEUDO CODE
 
+
+    //SERENA WILL WORK ON TOGGLE
     // Create toggleList method that adds and removes movie titles to the list when the user clicks + or -
 
         // *STRETCH GOAL* - filter movie list titles by alphabetical order, year, etc.
 
         // *STRETCH GOAL* - click on movie for more information
 
-    // Create init method on movieMarathonApp
-    movieMarathonApp.init = () => {
-        movieMarathonApp.searchMovie();
-        movieMarathonApp.searchMovieById();
-    }
-        // call searchMovie method
         // call getMovie method (forEach item in searchMovie array)
-        // call displayMovies method (for each object getMovie creates)
         // call toggleList method to allow for adding and removing movies from the list
-
-    // Call movieMarathonApp.init
-    movieMarathonApp.init();
+    
+    // DANA WILL WORK ON MOBILE FUNCTION(Menu Bar to go to movie list...)
